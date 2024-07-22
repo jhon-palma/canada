383a532702584b0d9498d5c0405033d1a58b2c1d
CRistian <crisdapu21@gmail.com>
2024-07-22 06:31:07 -0500

    fix static

--- immobilier/urls.py
+++ immobilier/urls.py
@@ -10,7 +10,9 @@ urlpatterns = [
     path('', include(('apps.users.urls','users'), namespace='users')),
     path('', include(('apps.web.urls','web'), namespace='web')),
     path('properties/', include(('apps.properties.urls','properties'), namespace='properties')),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+
+
